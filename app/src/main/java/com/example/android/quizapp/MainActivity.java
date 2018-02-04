4de2a278b49d1cbb0fc3d44b7019e85c9299eb27
snowbeast06 <gtrinks@gmail.com>
2018-02-04 11:13:02 -0800

    finished questions, prettied up formatting

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,11 +23,17 @@ public class MainActivity extends AppCompatActivity {
         radioGroup1 = (RadioGroup) findViewById(R.id.radioGroup1);
         radioGroup2 = (RadioGroup) findViewById(R.id.radioGroup2);
         radioGroup3 = (RadioGroup) findViewById(R.id.radioGroup3);
+        radioGroup4 = (RadioGroup) findViewById(R.id.radioGroup4);
+        radioGroup5 = (RadioGroup) findViewById(R.id.radioGroup5);
+        radioGroup6 = (RadioGroup) findViewById(R.id.radioGroup6);
 
     }
 
     public void calculateScore(View view) {
-        int chosenAnswer1, chosenAnswer2, chosenAnswer3;
+        int chosenAnswer1, chosenAnswer2, chosenAnswer3, chosenAnswer4, chosenAnswer5, chosenAnswer6;
+
+// we're going to check each radio group to see what button was checked, and store that button ID in the chosenAnswer variable
+        //  then we're going to pass the chosen answer into a checker method for each question
 
         chosenAnswer1 = radioGroup1.getCheckedRadioButtonId();
         checkQuestion1(chosenAnswer1);
@@ -37,6 +44,20 @@ public class MainActivity extends AppCompatActivity {
         chosenAnswer3 = radioGroup3.getCheckedRadioButtonId();
         checkQuestion3(chosenAnswer3);
 
+        chosenAnswer4 = radioGroup4.getCheckedRadioButtonId();
+        checkQuestion4(chosenAnswer4);
+
+        chosenAnswer5 = radioGroup5.getCheckedRadioButtonId();
+        checkQuestion5(chosenAnswer5);
+
+        chosenAnswer6 = radioGroup6.getCheckedRadioButtonId();
+        checkQuestion6(chosenAnswer6);
+
+        String toastText = getString(R.string.toastPart1) + " " + score + " " + getString(R.string.toastPart2);
+        Toast.makeText(this, toastText, Toast.LENGTH_LONG).show();
+
+        //reset the score so it can recalculate if they press the button again...
+        score = 0;
 
         Log.i("MainActivity.java", "in the calculateScore method ");
         Log.i("MainActivity.java", "" + chosenAnswer1 + " " + chosenAnswer2);
@@ -56,7 +77,6 @@ public class MainActivity extends AppCompatActivity {
             Log.i("MainActivity.java", "Checking the first answer");
             Log.i("MainActivity.java", "correct answer");
         }
-        return;
     }
 
     private void checkQuestion2(int chosenAnswer) {
@@ -67,10 +87,9 @@ public class MainActivity extends AppCompatActivity {
 
         if ((correctAnswer.getId()) == chosenAnswer) {
             score++;
-            Log.i("MainActivity.java", "Checking the first answer");
+            Log.i("MainActivity.java", "Checking the 2nd answer");
             Log.i("MainActivity.java", "correct answer");
         }
-        return;
     }
 
     private void checkQuestion3(int chosenAnswer) {
@@ -81,10 +100,49 @@ public class MainActivity extends AppCompatActivity {
 
         if ((correctAnswer.getId()) == chosenAnswer) {
             score++;
-            Log.i("MainActivity.java", "Checking the first answer");
+            Log.i("MainActivity.java", "Checking the 3rd answer");
             Log.i("MainActivity.java", "correct answer");
         }
-        return;
+    }
+
+
+    private void checkQuestion4(int chosenAnswer) {
+        // we'll hardcode the correct answers and have a different checker for each question
+        // in this case the correct answer is #3
+
+        RadioButton correctAnswer = (RadioButton) findViewById(R.id.radioGroup4Button3);
+
+        if ((correctAnswer.getId()) == chosenAnswer) {
+            score++;
+            Log.i("MainActivity.java", "Checking the 3rd answer");
+            Log.i("MainActivity.java", "correct answer");
+        }
+    }
+
+    private void checkQuestion5(int chosenAnswer) {
+        // we'll hardcode the correct answers and have a different checker for each question
+        // in this case the correct answer is #3
+
+        RadioButton correctAnswer = (RadioButton) findViewById(R.id.radioGroup5Button3);
+
+        if ((correctAnswer.getId()) == chosenAnswer) {
+            score++;
+            Log.i("MainActivity.java", "Checking the 3rd answer");
+            Log.i("MainActivity.java", "correct answer");
+        }
+    }
+
+    private void checkQuestion6(int chosenAnswer) {
+        // we'll hardcode the correct answers and have a different checker for each question
+        // in this case the correct answer is #3
+
+        RadioButton correctAnswer = (RadioButton) findViewById(R.id.radioGroup6Button3);
+
+        if ((correctAnswer.getId()) == chosenAnswer) {
+            score++;
+            Log.i("MainActivity.java", "Checking the 3rd answer");
+            Log.i("MainActivity.java", "correct answer");
+        }
     }
 
 
